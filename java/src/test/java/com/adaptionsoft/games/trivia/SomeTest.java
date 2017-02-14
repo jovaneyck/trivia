77602003975c6ea155aa6606d5d06f38cea78117
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-02-14 17:48:33 +0100

    Java golden master

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,29 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+import org.approvaltests.Approvals;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void golden_master() throws Exception {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(stream));
+
+		GameRunner runner = new GameRunner();
+		Random rng = new Random(123);
+
+		for(int i = 0; i < 100; i++){
+			System.out.println("Playing game "+i);
+			runner.PlayGame(rng);
+		}
+
+		Approvals.verify(stream.toString());
 	}
 }
